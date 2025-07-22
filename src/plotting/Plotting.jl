19b675c1f7754e718ca9b18255a9aca896de7125
ximtecs <michael.haahr101293@gmail.com>
2025-07-22 14:27:59 +0200

    [DEV] included new plot_line function in main program for export. Also exports new default color palette in default_color_palette variable

--- src/plotting/Plotting.jl
+++ src/plotting/Plotting.jl
@@ -1,2 +1,6 @@
+const default_color_palette = ["black","tab:blue","tab:red","tab:green","tab:orange",
+                        "tab:purple","tab:brown","tab:pink","tab:gray","tab:olive"]
 include("SingleHeatmap.jl")
 include("MultiHeatmap.jl")
+
+include("SingleLine.jl")
